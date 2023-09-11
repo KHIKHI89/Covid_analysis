47288b07c9d5fd0d1d6878c4686beea3f5dc1399
Khadidja <111190473+KHIKHI89@users.noreply.github.com>
2023-09-11 19:17:38 +0100

    Delete Covid.sql

--- Covid.sql
+++
@@ -1,8 +0,0 @@
-select * from new_one..human_resources
-
-UPDATE new_one..human_resources
-set birthdate = case 
-     when birthdate like '%/%' then date_format(str_to_date(birthdate,'%m/%d/%Y'), '%Y-%d-%m')
-	 when birthdate like  '%-%' then date_format(str_to_date(birthdate,'%m/%d/%Y'), '%Y-%d-%m')
-	 else NULL
-end 
